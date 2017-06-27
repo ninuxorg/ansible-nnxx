740ac386144a621ed6d792dd44d6d7b1bb0eeb82
MikySal78 <mikysal78@gmail.com>
2017-06-27 18:59:58 +0200

    script installa hotspot basilicata

---
+++ organizations/basilicata/root/hotspot.sh
@@ -0,0 +1,15 @@
+#/bin/sh
+TEMP=/tmp
+
+wget https://github.com/mikysal78/nodogsplash-basilicata/archive/master.zip
+opkg updatell
+
+opkg install unzip
+
+unzip master.zip -d $TEMP
+rm -rf master.zip
+sh $TEMP/nodogsplash-basilicata-master/install.sh
+
+
+echo "Quando ha finito tutto puoi cancellare questo file,"
+echo "magari dopo aver fatto un reboot."
